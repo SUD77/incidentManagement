f7126e0e74d8398c0de9657d8dcf534c90acd78f
SUD77 <jaiswalsudhanshu20@gmail.com>
2024-12-28 16:05:34 +0530

    FIX : resolved compilation issue, that was coming because of duplicate plugin

---
+++ src/main/java/com/incident/incidentManagement/service/mapper/IncidentManagementMapper.java
@@ -0,0 +1,18 @@
+package com.incident.incidentManagement.service.mapper;
+
+import com.incident.incidentManagement.dtos.UserDto;
+import com.incident.incidentManagement.entity.User;
+import org.mapstruct.Builder;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(builder = @Builder(disableBuilder = true),unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface IncidentManagementMapper {
+
+    IncidentManagementMapper INSTANCE= Mappers.getMapper(IncidentManagementMapper.class);
+
+    UserDto toUserDto(User user);
+
+    User toUser(UserDto userDto);
+}
